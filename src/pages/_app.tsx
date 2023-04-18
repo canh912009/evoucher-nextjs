714fb8026407e65631cb564ccfcb85d7267cccae
canhnx <nx.canh@altimedia.kr>
2023-04-18 17:55:00 +0700

    install package  isomorphic-fetch  es6-promise

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/styles/style.css'  //load bootstrap trước r css sau 
+import es6Promise from 'es6-promise'
+import fetch from 'isomorphic-fetch'
 
 import type { AppContext, AppProps } from 'next/app'
 import Head from 'next/head'
+import App from 'next/app'
 
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import { useMemo } from 'react'
+
+es6Promise.polyfill()
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   const hideFooter: boolean = useMemo(() => {
@@ -26,36 +31,49 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
 
   }, [router]);
 
-  return <div id='root'>
-    <Head>
-      <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
-      <meta name="keywords" content="HTML5 Template" />
-      <meta name="description" content="Cộng đồng chế ảnh ZendVN" />
-      <meta name="author" content="etheme.com" />
-      <title>Cộng đồng chế ảnh ZendVN</title>
-      <link rel="icon" href="/favicon.ico" />
+  return (
+    <div id='root'>
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1" />
+        <meta name="keywords" content="HTML5 Template" />
+        <meta name="description" content="Cộng đồng chế ảnh ZendVN" />
+        <meta name="author" content="etheme.com" />
+        <title>Cộng đồng chế ảnh ZendVN</title>
+        <link rel="icon" href="/favicon.ico" />
 
-      <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
-      <link rel="stylesheet" href="/fonts/font-awesome/css/font-awesome.css" />
-      <link rel="stylesheet" href="/fonts/emotion/style.css" />
-      {/* <link href="css/style.css" rel="stylesheet" /> bỏ comment này thì nó sẽ load trước bootstrap gây vỡ giao diện --> import ở dòng 2 */}
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
+        <link rel="stylesheet" href="/fonts/font-awesome/css/font-awesome.css" />
+        <link rel="stylesheet" href="/fonts/emotion/style.css" />
+        {/* <link href="css/style.css" rel="stylesheet" /> bỏ comment này thì nó sẽ load trước bootstrap gây vỡ giao diện --> import ở dòng 2 */}
 
-      {/* JAVA SCRIPT */}
-      {/* require */}
-      {/*  */}
-      {/* HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries */}
-      {/*[if lt IE 9]>
+        {/* JAVA SCRIPT */}
+        {/* require */}
+        {/*  */}
+        {/* HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries */}
+        {/*[if lt IE 9]>
 	      <![endif]*/}
-    </Head>
+      </Head>
 
-    {!hideHeader && <Header />}
+      {!hideHeader && <Header />}
 
-    <main>
-      <Component {...pageProps} />
-    </main>
+      <main>
+        <Component {...pageProps} />
+      </main>
 
-    {!hideFooter && <Footer />}
-  </div>
+      {!hideFooter && <Footer />}
+    </div>
+  )
+}
+
+// appContext khác với các Pages context
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+
+  return {
+    pageProps: {
+      ...appProps.pageProps,
+    }
+  }
 }
