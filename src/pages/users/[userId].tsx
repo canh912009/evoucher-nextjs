da2b2a198e138b8e232b91fc2850e7842487cf43
canhnx <nx.canh@altimedia.kr>
2023-04-18 15:42:46 +0700

    user detail

---
+++ src/pages/users/[userId].tsx
@@ -0,0 +1,14 @@
+import UserDetailInfo from '@/components/UserDetailInfo'
+import UserDetailPost from '@/components/UserDetailPost'
+import React from 'react'
+
+const UserDetail = () => {
+    return (
+        <div className="container">
+            <UserDetailInfo />
+            <UserDetailPost />
+        </div>
+    )
+}
+
+export default UserDetail 
