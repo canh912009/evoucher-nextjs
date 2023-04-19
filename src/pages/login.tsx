2948eb2bd6f4bf1f2770a1437e749c23450850e9
canhnx <nx.canh@altimedia.kr>
2023-04-19 09:53:02 +0700

    Thu thập dữ liệu từ Form Logi

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { type } from 'os'
+import React, { useState } from 'react'
+
+type FormLogin = {
+    email: string,
+    password: string
+}
+
+const initFormLogin: FormLogin = {
+    email: '',
+    password: ''
+}
 
 const Login = () => {
+    const [formData, setFormData] = useState(initFormLogin);
+
+    // function handleOnChange(key: string) {
+    //     return (evt: any) => {
+    //         setFormData({
+    //             ...formData, // lấy data từ cả form cũ và chỉ sửa data Email dưới
+    //             [key]: evt.target.value
+    //         });
+    //     }
+    // }
+
+    const handleOnChange = (key: string) => (evt: any) => {  //function return a function , t.tự hàm trên
+        setFormData({
+            ...formData, // lấy data từ cả form cũ và chỉ sửa data Email dưới
+            [key]: evt.target.value
+        });
+    }
+
     return (
         <div className="ass1-login">
             <div className="ass1-login__logo">
@@ -10,11 +39,14 @@ const Login = () => {
                 <p>Đăng nhập</p>
                 <div className="ass1-login__form">
                     <form action="#">
-                        <input type="text" className="form-control" placeholder="Email" required />
-                        <div className="ass1-input-copy">
-                            <input type="password" className="form-control" placeholder="Mật khẩu" required />
-                            <a href="#">Copy</a>
-                        </div>
+                        <input
+                            value={formData.email}
+                            onChange={handleOnChange('email')}
+                            type="text" className="form-control" placeholder="Email" required />
+                        <input
+                            value={formData.password}
+                            onChange={handleOnChange('password')}
+                            type="password" className="form-control" placeholder="Mật khẩu" required />
                         <div className="ass1-login__send">
                             <a href="dang-ky.html">Đăng ký một tài khoản</a>
                             <button type="submit" className="ass1-btn">Đăng nhập</button>
