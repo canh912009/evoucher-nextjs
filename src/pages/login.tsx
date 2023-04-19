6ac34bd979d58332918ab27e7fbc9a0039065299
canhnx <nx.canh@altimedia.kr>
2023-04-19 13:28:51 +0700

    call api login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import { type } from 'os'
-import React, { useState } from 'react'
+import React, { FormEvent, useState } from 'react'
+import api from '@/services/api'
 
 type FormLogin = {
     email: string,
@@ -23,11 +24,31 @@ const Login = () => {
     //     }
     // }
 
-    const handleOnChange = (key: string) => (evt: any) => {  //function return a function , t.tự hàm trên
-        setFormData({
-            ...formData, // lấy data từ cả form cũ và chỉ sửa data Email dưới
-            [key]: evt.target.value
-        });
+    // const handleOnChange = (key: string) => (evt: any) => {  //function return a function , t.tự hàm trên
+    //     setFormData({
+    //         ...formData, // lấy data từ cả form cũ và chỉ sửa data Email dưới
+    //         [key]: evt.target.value
+    //     });
+    // }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+        event.preventDefault;
+        console.log("formData", formData);
+        api.callJson("/member/login.php", formData, "POST")
+            .then(
+                data => {
+                    console.log("data", data);
+                }
+            )
+    }
+
+    function handleOnChange(key: string): React.ChangeEventHandler<HTMLInputElement> | undefined {
+        return (evt: any) => {
+            setFormData({
+                ...formData, // lấy data từ cả form cũ và chỉ sửa data Email dưới
+                [key]: evt.target.value
+            });
+        }
     }
 
     return (
@@ -38,7 +59,7 @@ const Login = () => {
             <div className="ass1-login__content">
                 <p>Đăng nhập</p>
                 <div className="ass1-login__form">
-                    <form action="#">
+                    <form action="#" onSubmit={handleSubmit}>
                         <input
                             value={formData.email}
                             onChange={handleOnChange('email')}
---
+++ src/services/api.ts
@@ -0,0 +1,31 @@
+import { BASE_URL } from '@/constants'
+import fetch from 'isomorphic-fetch'
+
+// Trong TypeScript, Record là một kiểu dữ liệu giúp định nghĩa một đối tượng với các cặp key-value
+const api = {
+    callJson: async (path: string, data: Record<string, any>, method: string = "GET") => {
+        const _url = `${BASE_URL}${path}`;
+        const config: RequestInit = {
+            method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        return fetch(_url, config).then(res => res.json());
+    },
+    callWithAuth: async (path: string, data: Record<string, any>, method: string = "GET") => {
+        const _url = `${BASE_URL}${path}`;
+        const config: RequestInit = {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                "Authentication": "Bearer token-cookie",
+            },
+            body: JSON.stringify(data)
+        }
+        return fetch(_url, config).then(res => res.json());
+    }
+}
+
+export default api;
